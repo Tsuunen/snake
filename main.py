5a1bffc56b68482546928ee83922deb48c431ce9
LAFORGUE Remi <laforgueremi@gmail.com>
2024-05-28 19:11:31 +0200

    apple fonctionelle mais peut spawn dans le serpent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from random import randint
 
 
 pygame.init()
@@ -20,7 +21,7 @@ class Snake():
         self.tete[1] += 20 * self.y_direction
 
         self.body.pop(0)
-        self.body.append(pygame.Rect(self.tete[0] % 800, self.tete[1] % 600, 20, 20))
+        self.body.append(pygame.Rect(self.tete[0] % SCREEN_WIDTH, self.tete[1] % SCREEN_HEIGHT, 20, 20))
 
     def add_cell(self):
         last_cell = self.body[0]
@@ -30,12 +31,31 @@ class Snake():
         for rect in self.body:
             pygame.draw.rect(self.screen, "white", rect)
 
+
+class Apple:
+    def __init__(self, screen) -> None:
+        self.screen = screen
+        self.coords = (randint(0, SCREEN_WIDTH // 20), randint(0, SCREEN_HEIGHT // 20))
+        self.apple = pygame.Rect(self.coords[0] * 20, self.coords[1] * 20, 20, 20)
+
+    def display(self):
+        self.apple.x = self.coords[0] * 20
+        self.apple.y = self.coords[1] * 20
+        pygame.draw.rect(self.screen, "red", self.apple)
+
+    def change_coords(self):
+        self.coords = (randint(0, SCREEN_WIDTH // 20), randint(0, SCREEN_HEIGHT // 20))
+
+         
+
+
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Snake")
         self.clock = pygame.time.Clock()
         self.player = Snake(self.screen)
+        self.apple = Apple(self.screen)
 
     def run(self):
         running = True
@@ -70,10 +90,16 @@ class Game:
             if pygame.Rect.collidelist(self.player.body[-1], self.player.body[:-1]) != -1:
                 self.player.__init__(self.screen)
 
+            if pygame.Rect.colliderect(self.player.body[-1], self.apple.apple):
+                self.player.add_cell()
+                self.apple.change_coords()
+
             # Dessiner tout à l'écran
             self.screen.fill((0, 0, 0))
+            self.apple.display() 
             self.player.display()
             pygame.display.flip()
+
 
             # if pygame.Rect.collidelist(self.player.body[-1], self.player.body[:-1]) != -1:
             #     self.player.__init__(self.screen)
